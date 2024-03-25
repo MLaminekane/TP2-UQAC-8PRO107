921710a245bccb9601ab2c4a027962405ad5fbd5
MLaminekane <mlaminekane01@gmail.com>
2024-03-25 16:04:22 -0400

    finalisation du projet

--- TP2/Source.cpp
+++ TP2/Source.cpp
@@ -2,8 +2,35 @@
 #include <cmath>
 #include <string>
 #include <random>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+struct Livre {
+    string titre;
+    int anneeEdition;
+    int nbPages;
+    double prixAchat;
+    double prixVente;
+    int nbCopiesAchete;
+    int nbCopiesVendues;
+};
+
+bool estBonVendeur(const Livre& livre) {
+    return (livre.prixVente * livre.nbCopiesVendues) - (livre.prixAchat * livre.nbCopiesAchete) >= 10000.0;
+}
+
+struct Point {
+    char identifiant;
+    double x;
+    double y;
+};
+
+double distance(const Point& p1, const Point& p2) {
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
 
 void afficherMenu() {
     cout << "=== Menu TP2 ===" << endl;
@@ -113,7 +140,7 @@ int main() {
             float moyenne = 0;
             for (int i = 0; i < 5; i++)
             {
-                cout << "Saisir le nombre " << i + 1 << endl;
+                cout << "Saisir le nombre " << i + 1 << ": " << endl;
                 cin >> nb;
                 tabSave[i] = nb;
                 moyenne += nb;
@@ -132,7 +159,7 @@ int main() {
             break;
         }
 
-        case 4:{
+        case 4: {
             for (int x = 50; x <= 100; x++)
             {
                 bool Primer = true;
@@ -196,14 +223,70 @@ int main() {
         }
 
         case 11: {
-
+              string anneeStr;
+              cout << "Entrez une annee : ";
+              cin >> anneeStr;
+              int annee;
+              stringstream(anneeStr) >> annee; 
+              ifstream fichier("Livres.dat");
+              if (!fichier) {
+                  cout << "Erreur: Impossible d'ouvrir le fichier Livres.txt" << endl;
+                  return 1;
+              }
+              string ligneActuelle, titreLivre;
+              bool trouve = false;
+              while (getline(fichier, titreLivre)) {
+                  getline(fichier, ligneActuelle);
+                  stringstream ss(ligneActuelle);
+                  int anneeEdition;
+                  ss >> anneeEdition;
+                  if (anneeEdition == annee) {
+                      Livre livre;
+                      livre.titre = titreLivre;
+                      livre.anneeEdition = anneeEdition;
+                      ss >> livre.nbPages >> livre.prixAchat >> livre.prixVente >> livre.nbCopiesAchete >> livre.nbCopiesVendues;
+                      if (estBonVendeur(livre)) {
+                          if (!trouve) {
+                              cout << "Livres publies en " << annee << " : " << endl;
+                              trouve = true;
+                          }
+                          cout << " - " << livre.titre << endl;
+                      }
+                  }
+              }
+              if (!trouve) {
+                  cout << "Aucun bon vendeur trouve pour l'annee " << annee << endl;
+              }
+              fichier.close();
+              break;
         }
 
         case 13: {
+            ifstream fichier("Polygone.dat");
+            if (!fichier) {
+                cerr << "Erreur: Impossible d'ouvrir le fichier Polygone.txt" << endl;
+                return 1;
+            }
 
+            const int MAX_POINTS = 26;
+            Point points[MAX_POINTS];
+            int cpt = 0;
+
+            char identifiant;
+            double x, y;
+            while (fichier >> identifiant >> x >> y) {
+                points[cpt++] = { identifiant, x, y };
+            }
+            fichier.close();
+            double perimetre = 0.0;
+            for (int i = 0; i < cpt - 1; ++i) {
+                perimetre += distance(points[i], points[i + 1]);
+            }
+            perimetre += distance(points[cpt - 1], points[0]);
+            cout << "Le perimetre du polygone est : " << perimetre << endl;
+            break;
         }
 
-        
         case 0: {
             cout << "Au revoir !" << endl;
             break;
